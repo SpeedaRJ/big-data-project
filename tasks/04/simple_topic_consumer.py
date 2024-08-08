c47514ec45dd22f04f7f60ae4057594c46c7c3d3
Luka Škodnik <luka.skodnik@gmail.com>
2024-08-08 18:57:03 +0200

    Include street code1 in the streaming data

--- tasks/04/simple_topic_consumer.py
+++ tasks/04/simple_topic_consumer.py
@@ -11,23 +11,23 @@ BOOTSTRAP_SERVERS = "localhost:29092"
 def parse_args():
     parser = argparse.ArgumentParser(description="Consume data from Kafka")
     parser.add_argument("--topics", nargs="+", help="Topics to consume")
-    parser.add_argument("--save", default=False, help="Save data to file")
+    parser.add_argument("--save_path", type=str, default="./tasks/04/results", help="Path to save the results")
     return parser.parse_args()
 
 
-def consumer(topic, save):
+def consumer(topic, save_path):
     # start the consumer
     consumer = KafkaConsumer(topic, bootstrap_servers=BOOTSTRAP_SERVERS, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
     
     # delete the file with results if it exists
-    if save and os.path.exists(f"{topic}.txt"):
-        os.remove(f"{topic}.txt")
+    if save_path and os.path.exists(f"{save_path}/{topic}.txt"):
+        os.remove(f"{save_path}/{topic}.txt")
     
     # consume messages
     print(f"Consumer started. Waiting for messages on topic {topic}...")
     for message in consumer:
-        if save:
-            with open(f"{topic}.txt", "a") as f:
+        if save_path:
+            with open(f"{save_path}/{topic}.txt", "a") as f:
                 f.write(f"{message.topic}:{message.partition}:{message.offset} - key: {message.key}, value: {message.value}\n")
         else:
             print(f"{message.topic}:{message.partition}:{message.offset} - key: {message.key}, value: {message.value}")
@@ -37,7 +37,7 @@ def main():
     args = parse_args()
     # start a separate process for each topic
     for topic in args.topics:
-        p = Process(target=consumer, args=(topic, args.save))
+        p = Process(target=consumer, args=(topic, args.save_path))
         p.start()
 
 
