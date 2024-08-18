f0e6e6426920574d302e1c53cc10b3fd734168d3
Luka Škodnik <luka.skodnik@gmail.com>
2024-08-18 21:17:46 +0200

    ML update

--- data_scripts/ML/ml_dask.py
+++ data_scripts/ML/ml_dask.py
@@ -1,7 +1,13 @@
-from dask_ml.linear_model import LinearRegression
+from dask_ml.linear_model import LinearRegression, LogisticRegression
 
 
 def make_fit_predict(X, y_train, X_test):
     lr = LinearRegression(solver_kwargs={"normalize":False})
     lr.fit(X, y_train)
     return lr.predict(X_test)
+
+
+def make_fit_predict_classification(X, y_train, X_test):
+    lr = LogisticRegression(solver_kwargs={"normalize":False})
+    lr.fit(X, y_train)
+    return lr.predict(X_test), lr.predict_proba(X_test)
--- data_scripts/ML/ml_sklearn.py
+++ data_scripts/ML/ml_sklearn.py
@@ -1,4 +1,4 @@
-from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPRegressor, MLPClassifier
 
 
 def make_fit_predict(X, y_train, X_test):
@@ -16,3 +16,20 @@ def make_fit_predict(X, y_train, X_test):
     for X_batch, y_batch in zip(X_batches, y_batches):
         clf.partial_fit(X_batch, y_batch)
     return clf.predict(X_test)
+
+
+def make_fit_predict_classification(X, y_train, X_test):
+    batch_size = 10
+    X_batches = [X[i : i + batch_size] for i in range(0, len(X), batch_size)]
+    y_batches = [
+        y_train[i : i + batch_size] for i in range(0, len(y_train), batch_size)
+    ]
+    clf = MLPClassifier(
+        max_iter=1000,
+        tol=1e-3,
+        random_state=42,
+        hidden_layer_sizes=(128, 256, 512, 256, 128),
+    )
+    for X_batch, y_batch in zip(X_batches, y_batches):
+        clf.partial_fit(X_batch, y_batch, classes=[0, 1])
+    return clf.predict(X_test), clf.predict_proba(X_test)
